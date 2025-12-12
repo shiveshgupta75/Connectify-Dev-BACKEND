6c269abd38d3d975e4f9644867154c04d7e2d79c
Shivesh Gupta <shiveshgptt.1234@gmail.com>
2025-12-12 18:08:57 +0530

    Routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express=require('express');
 const app=express();
-  app.use((req,res)=>{
-    res.send("HELLO WORLD!! This is Connectify Backend Server.");
-})
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello2 hello2 hello2");
+});
+app.use("/hello/2",(req,res)=>{
+    res.send("Hello hello hello");
+});
+
+app.use("/",(req,res)=>{
+    res.send("Welcome to Connectify API");
+});
+app.use("/login",(req,res)=>{
+    res.send("login route");
+});
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
