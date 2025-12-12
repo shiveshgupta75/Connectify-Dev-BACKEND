289b7fc18141b78a764d43dccbe17ab3a695bc1e
Shivesh Gupta <shiveshgptt.1234@gmail.com>
2025-12-12 19:34:46 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 const express=require('express');
 const app=express();
 
+//GET
+app.get("/user",(req,res)=>{
+    res.send("GET DATA FROM SERVER");
+
+});
+
+app.get("/data",(req,res)=>{
+    console.log(req.query)
+    res.send("GET DATA FROM SERVER");
+
+});
+
+app.get("/username/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
+    res.send({firstname:"Shivesh",lastname:"Gupta"});
+
+});
+//POST
+app.post("/user",(req,res)=>{
+    console.log(req.body);
+    res.send("POST Call for /user!! Data saved successfully");
+});
+//DELETE
+app.delete("/user",(req,res)=>{
+    console.log("Data deleted successfully");
+    res.send("DELETE Call for /user");
+});
 app.use("/hello",(req,res)=>{
     res.send("Hello2 hello2 hello2");
 });
