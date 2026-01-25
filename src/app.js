f6e24f8c904446f2257cbc5c2c2610bd803aca41
Shivesh Gupta <shiveshgptt.1234@gmail.com>
2026-01-26 01:15:53 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -2,14 +2,10 @@ const express=require('express');
 const connectDB=require("./config/database");
 const User=require("./models/user"); 
 const app=express();
-
+ app.use(express.json());
 app.post("/signup",async(req,res)=>{
-       const user=new User({
-        firstName:"MS",
-        lastName:"DHONI",
-        emailId:"msd23@gmail.com",
-        password:"msd123"
-       });
+      const user=new User(req.body);
+    
  try{
     await user.save();
   res.send("User Added Successfully");
