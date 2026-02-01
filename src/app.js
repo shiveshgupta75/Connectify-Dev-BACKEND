9fc7af589efca31446e383b945eef3c8cc1b38c1
Shivesh Gupta <shiveshgptt.1234@gmail.com>
2026-02-01 20:04:56 +0530

    user routes created and learn about Ref,Populate uses

--- src/app.js
+++ src/app.js
@@ -9,10 +9,12 @@ const cookieParser=require("cookie-parser");
  const authRouter=require("./routes/auth");
  const profileRouter=require("./routes/profile");
  const requestRouter=require("./routes/request");
+ const userRouter=require("./routes/user");
 
  app.use("/",authRouter);
  app.use("/",profileRouter);
    app.use("/",requestRouter);
+   app.use("/",userRouter);
  
 
 
--- src/routes/request.js
+++ src/routes/request.js
@@ -55,4 +55,35 @@ requestRouter.post("/request/send/:status/:toUserId",UserAuth,async(req,res)=>{
   
 });
 
+requestRouter.post("/request/review/:status/:requestId",UserAuth,async(req,res)=>{
+  try{
+    const loggedInUser=req.user;
+    const {status,requestId}=req.params;
+
+    const allowedStatus=["accepted","rejected"];
+    if(!allowedStatus.includes(status)){
+      return res.status(400).json({message: "Status not allowed!!"});
+
+    }
+
+    const connectionRequest=await ConnectionRequest.findOne({
+      _id:requestId,
+      toUserId:loggedInUser._id,
+      status:"interested",
+    });
+    if(!connectionRequest){
+      return res.status(404).send({message:"Connection Request Not Found",
+        success:false,
+      });
+    }
+    connectionRequest.status = status;
+    const data=await connectionRequest.save();
+    res.json({message: " Connection Request "+ status,data});
+   
+
+  }catch(err){
+    res.status(400).send("ERROR: "+ err.message);
+  }
+});
+
 module.exports=requestRouter;
